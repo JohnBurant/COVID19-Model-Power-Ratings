e4268b977981ab471d3d6d0f3a69cadf8c91790f
JB <tabouli@hummus.local>
2020-08-15 20:58:34 +0200

    initial upload

--- README.md
+++ README.md
@@ -1,2 +1,99 @@
-# COVID19-Model-Power-Ratings
-Compares accuracy of COVID19 models.
+# COVID19 Model Power Ratings
+
+COVID19 Model Power Ratings provide a framework for comparing the accuracy of the models submitted to @reichlab's COVID19 Forecast Hub (https://covid19forecasthub.org/) as a ***single number***.
+
+At present, the power ratings are built from the error measures calculated by @youyanggu's Evaluation of COVID-19 Models (https://github.com/youyanggu/covid19-forecast-hub-evaluation/) package. That package provides a variety of error measures, but does not consolidate them into a single number. The measures in @youyanggu's package measure only the accuracy of forecasted deaths and therefore the power ratings here are also limited to forecasted deaths. A power ratings calculations does not, by itself, require the calculation of any error measures. In fact the code presented here simply reads the error measures contained in @youyanggu's covid19-forecast-hub-evaluation output files and uses them as input to further calculations.
+
+The power rating framework provides the flexibility to compare models over any desired time range and time horizon, to include or exclude any particular models, and to use  any desired set of error measures. This flexibility enables both high-level summary ratings and more detailed analyses of the accuracy of the models.
+
+### Desired Attributes of a Comparative Measure ###
+
+A method that compares the accuracy of these models over time should:
+* Provide a consistent numerical scale from week to week.
+* Provide an output numerical measure that describes only the models' performance relative to each other, not to absolute performance. (Although models are assessed on the basis of their absolute performance as input the calculation of comparative measures, the final numbers returned as comparative measures should depend only *indirectly* on the absolute performance. Stated another way: If all models perform equally relatively well or relatively poorly on an absolute measure, the measure of their relative performance should not change.)
+* Consider performance over the full time range of a forecast's time horizon, e.g., if the time horizon of a forecast is 4 weeks, the evaluation should consider not just the 4-week period, but all intermediate periods (1-week, 2-week, 3-week and 4-week) periods over that time horizon.
+* Consider multiple geographical levels of aggregation, e.g., both national & state level. It should take into account that average error comparisons across multiple geographies are by definition skewed towards performance in the most populous geographies (technically those geographies with the highest number of deaths).
+* Distinguish between relatively close performance and relatively disparate performance (i.e., it should not be a simple ranking).
+
+### Power Rating ###
+
+The power rating framework has these attributes.
+
+For any particular *individual* error measure applied to a set of models, the power rating scoring is as follows:
+
+* The model with the minimum error is assigned a power rating of 100.
+* The model with the median error is assigned a power rating of 50.
+* Other models are assigned a power rating on a proportional basis, e.g.
+  * A model with an error midway between the minimum error and the median error is assigned a power rating of 75.
+  * A model with an error 1.5x the difference between the minimum error and the median error is assigned a power rating of 25.
+  * A model with an error 2x the difference between the minimum error and the median error is assigned a power rating of 0.
+* A power rating score cannot be less than 0. Therefore any model with an error >2x the difference between the minimum error and the median error is also assigned a power rating of 0.
+
+Truncating the low-end ratings to be 0 at 2x the difference between the minimum error and the median error ensures that the power ratings are not skewed by the inclusion of highly inaccurate (outlier) models in the set.
+
+### Choices ###
+
+The power rating as described above is a generic scheme that could be used in many settings. Applying it to the models on the COVID19 Forecast Hub site, however, requires making choices of certain parameters and inclusion/exclusion policies.
+
+*Parameters*
+
+Time frame: Model forecasts included in the COVID19 Forecast Hub are submitted and evaluated on a weekly cycle. The earliest forecasts date to week 17 of 2020 (the week ending 2020-04-25). By default, the code here includes all forecasts from this week forward.
+
+Forecast time horizon: Forecasts submitted to the COVID19 Forecast Hub typically have a time horizon of at least four weeks from the date of submission (including forecasts for that same week plus three additional weeks). Some models includes forecasts for a longer time horizon. The default time horizon used by the code here is 4 weeks.
+
+*Measures Included*
+
+The default set of measures taken from @youyanggu's Evaluation of COVID-19 Models package are:
+
+* Mean absolute error (state-level)
+* Root-mean-square error (state-level) (note: @youyanggu provides square errors, the code here takes the square root of these)
+* Mean ranking (state-level)
+* Absolute percent error (national)
+
+By virtue of including three state-level measures, the default power rating emphasizes state-level accuracy over national-level accuracy. Because of the disparate levels of impact of COVID19 across the country and the value of having localized forecasts, this is a reasonable choice. The highly uneven population distribution among the states, however, means that any average of state-level measures will be skewed towards performance in the more populous states.
+The inclusion of the mean ranking (i.e., the averaged ranking of the models in each of the states) rewards consistency across states and offsets this issue somewhat.
+The inclusion of the absolute error at the national level provides credit for models that may not be highly accurate for all states, but for which cancellation of errors leads to accuracy at the national level.
+
+*Model Inclusion/Exclusion*
+
+The power ratings are inherently dependent on the set of models included in the ratings. Several options are permitted by the current code:
+
+* Whether or not to include baseline models (linear projections). The default is to include these.
+* Whether or not to include the COVID19 Forecast Hub ensemble model, which itself is an aggregation of the other models. The default is to include the ensemble.
+* Whether or not to include models for which the full set of error measures are not available. Most models provide projections at the state level, which are then aggregated to the national level. However, some models only provide national forecasts, and thus are missing 3 of the 4 error measures. Models that provide only national forecasts will themselves receive low overall power scores (they are assigned a 0 for missing measures). However, it is meaningful include them in order to assess the national-level aggregation of the multi-state models against both state-level and national-only models. The default is to include these models.
+* Whether or not to include models that do not provide forecasts for all weeks in the desired time horizon. To compare models with each other consistently, it's reasonable to require models to have projections available for the identical number of weeks. Therefore the default is not to include models that do not provide projections for all weeks in the desired time horizon.
+* Whether or not to generate results for recent weeks. The power ratings are assigned on the basis of performance over a time horizon of specified number of weeks. This policy choice determines whether (preliminary) power ratings are generated for model forecasts prior to reaching the end of that time horizon. The default is not to generate these preliminary ratings.
+
+### Process ###
+
+The first power ratings calculated are for each start_week/num_week/measure/model 4-tuple (where num_week is the 0, 1, 2, etc. week in the desired time horizon). To calculate these power ratings, all models are compared across every start_week/num_week/measure 3-tuple.
+
+The 4-tuple power ratings are then aggregated by straight averaging in a variety of ways to calculate the aggregated power ratings.
+
+The feature measures of this code are:
+
+**Lifetime power rating**: Provides a single number to compare each model with all other models across its whole lifetime, for the chosen set of error measures and time horizon.
+
+**Weekly power rating**: Provides a weekly measure for each model, for each week in which a model forecast was provided, comparing it against all other models for the chosen set of error measures and time horizon.
+
+### Code ###
+
+The power rating scheme is implemented in Python. The code can be found in power_ratings_v0.2.py, in the root directory of this repo. The code requires a local copy of the /evaluation subdirectory of https://github.com/youyanggu/covid19-forecast-hub-evaluation.
+
+### Results ###
+
+The python code generates a multi-tab Excel file, which can be found in the /results subdirectory of this repo.
+
+*Info:* Summarizes parameters used in the power rating calculation and provides a datestamp.
+
+*Model_lifetime:* The lifetime power ratings as described above.
+
+*Model_weekly:* The weekly power ratings as described above.
+
+*Model_measures:* Provides a comparison of each model on each error measure over its whole lifetime. This shows which models do well, or not well, on each measure. Some are relatively consistent across all measures, others vary widely.
+
+*Model_num_weeks:* Provides a comparison of each model by the number of weeks out from the week of submission. This shows how well models perform over the course of the time horizon. Some models improve over the course of the time horizon, whereas others get worse.
+
+*Model_week_pairs:* Provide a power rating for a model for each week throughout the time horizon, separately for each week in which a forecast was submitted.
+
+*Raw_power_ratings:* The power ratings for the 4-tuple of model/start_week/end_week/measure described above, which form the basis for all power rating aggregations.
---
+++ power_ratings_v0.2.py
@@ -0,0 +1,282 @@
+# power_ratings_v0.2py
+# last updated: 15 August 2020
+# https://github.com/JohnBurant/COVID19-Model-Power-Ratings
+
+# No command-line options, sorry. Just edit params and policy choices in the code.
+
+# Timeframe choices
+first_week = 17
+last_week = 32
+nweeks = 3 # Input as 0-indexed, i.e., how many we actually want - 1
+
+# Policy choices
+# See discussion on github site
+d_policy = {'include_baselines': True,
+            'include_ensemble' : True,
+            'include_models_without_full_measure_set': True,
+            'include_models_without_all_weeks': False,
+            'generate_partial_results_for_recent_weeks': False
+           }
+
+# Where to find and put things
+working_dir = '/Users/tabouli/Dropbox/Personal/Corona/power_ratings/COVID19-Model-Power-Ratings/' # Local path of this repo
+yyg_repo_dir = '/Users/tabouli/Dropbox/Personal/Corona/youyanggu/covid19-forecast-hub-evaluation/' # path to clone of https://github.com/youyanggu/covid19-forecast-hub-evaluation
+eval_dir = yyg_repo_dir + 'evaluations/'
+output_dir = working_dir + 'results/'
+
+import pandas as pd
+import numpy as np
+import epiweeks
+import datetime
+
+def power_score(s_in):
+    '''
+    Given a pd.Series of error measures, return a (sorted) pd.Series w/ power ratings.
+    Power rating is as follows:
+      The minimum error is assigned a power rating of 100;
+      The median error is assigned a power rating of 50;
+      Remaining errors are distributed proportionately according to how they fall on the scale defined by min->100, median->50;
+      Any errors that would result in a power rating < 0 are set = 0.
+    '''
+    s = s_in.sort_values()
+    err_range = 2*(s.median()-s.min())
+    s_scaled = (err_range - s + s.min()).clip(0)
+    s_scaled = 100*s_scaled/s_scaled[0]
+    return s_scaled
+
+def say(st,l_outs,save=True,term='\n'):
+    '''
+    Given a string st, a list l_outs, a boolean save and an options string term:
+      print st, using term asthe argument end of print function
+      if save=True append st to l_outs
+    '''
+    print(st,end=term)
+    if save: l_outs.append(st)
+
+l_outs = [] # Accumulates outputs in order to store in final .xslx file
+
+say('Beginning generation of power ratings from raw evaluation files.',l_outs)
+datestamp = datetime.datetime.today().strftime('%Y-%m-%d')
+outstring = 'Datestamp: '+datestamp
+say(outstring,l_outs)
+say('Following these policies:',l_outs)
+for k, v in d_policy.items():
+    outstring = '  '+str(k)+': '+str(v)
+    say(outstring,l_outs)
+
+# Create some handy dicts to move between YYYY-MM-DD and epiweek
+d_epiweeks = {i: epiweeks.Week(2020,i) for i in range(first_week,last_week+1)}
+d_wk_to_enddate = {k: w.enddate().strftime('%Y-%m-%d') for k, w in d_epiweeks.items()}
+d_enddate_to_wk = {v: k for k, v in d_wk_to_enddate.items()}
+d_wk_to_startdate = {k: (w.startdate()+datetime.timedelta(days=1)).strftime('%Y-%m-%d') for k, w in d_epiweeks.items()}
+d_startdate_to_wk = {v: k for k, v in d_wk_to_startdate.items()}
+
+# For each starting week, create list of week pairs that are (starting_week,ending_week). Store as dict.
+d_week_pairs = {i: [(j,i) for j in range(first_week,last_week+1) if i >= j] for i in range(first_week,last_week+1)}
+
+# Also do this with the full string YYYY-MM-DD version
+d_week_pairs_str = {}
+for k, v in d_week_pairs.items():
+    d_week_pairs_str[d_wk_to_enddate[k]] = [d_wk_to_startdate[tup[0]]+'_'+d_wk_to_enddate[tup[1]] for tup in v]
+
+# Read in all the files in the evaluations directory
+# We could just glob but curating it slightly might make the manipulations easier
+# Note we make the switch from YYYY-MM-DD to integer weeks right here.
+
+# Format {filename_base: {'cols': list of columns to keep, 'short_name': what we call the measure}
+d_files_measures_cols = {'states_abs_errs': {'cols' : ['mean'], 'short_name': 'state_abs'},
+                         'states_sq_errs': {'cols' : ['mean'], 'short_name': 'state_sq'},
+                         'states_mean_ranks': {'cols' : ['mean_rank'], 'short_name': 'state_rank'},
+                         'us_errs': {'cols': ['perc_error'], 'short_name': 'us_tot'}
+                        }
+
+measures_used = d_files_measures_cols.keys()
+measures_used_str = 'Measures used: '+', '.join(list(measures_used))
+say(measures_used_str,l_outs)
+say('Loading evaluation files...',l_outs,term='')
+l_dfs = []
+for meas, vd in d_files_measures_cols.items():
+    cols_to_keep = vd['cols']
+    short_name = vd['short_name']
+    for k, v in d_week_pairs_str.items():
+        for pr in v:
+            fstring = k+'/'+pr+'_'+meas+'.csv'
+            df = pd.read_csv(eval_dir+fstring)
+            df = df[['Unnamed: 0', *cols_to_keep]]
+            df = df.set_index('Unnamed: 0')
+            date_pr = pr.split('_')
+            int_weeks = [d_startdate_to_wk[date_pr[0]], d_enddate_to_wk[date_pr[1]]]
+            cols_mi = pd.MultiIndex.from_tuples([(short_name+'_'+c, int_weeks[0], int_weeks[1]) for c in cols_to_keep],
+                                           names=['measure','start_wk','end_wk'])
+            df.columns = cols_mi
+            l_dfs.append(df)
+
+say('Done',l_outs,save=False)
+outstring = 'Number of files processed: '+str(len(l_dfs))
+say(outstring,l_outs)
+
+df_raw = pd.concat(l_dfs,axis=1)
+df_raw = df_raw.dropna(axis=0,how='all') # Some models show up but not for the metric we use (notably: UChicago, which did only Illinois)
+outstring = 'Shape of df_raw: '+str(df_raw.shape)
+say(outstring,l_outs)
+
+# Do some further processing into a new df
+#   df will be the main dataframe we'll work with that will contain start_wk/num_wk/model/measure (4-tuple) error measures
+
+# Switch from first_wk/end_wk to first_wk/num_wks where num_wks counts how many weeks out from first_wk the predictions are, starting w/ 0.
+df = df_raw.copy()
+df = df.T.reset_index()
+df['num_wks'] = df['end_wk'] - df['start_wk']
+df = df.drop(columns=['end_wk'])
+df = df.set_index(['start_wk','num_wks','measure'])
+df = df.sort_index()
+
+# Also, slight processing:
+# (1) convert mean-square-errors into root-mean-square-errors (and rename the index)
+# (2) take absolute value of percentage error on whole-US projection
+
+# (1a) apply the sqrt
+screen_sq_errs = df.index.get_level_values('measure').str.startswith('state_sq')
+df.loc[screen_sq_errs,:] = df.loc[screen_sq_errs,:].applymap(np.sqrt)
+
+# (1b) rename the measure; easier to do this way than dealing with the index object itself!
+df = df.reset_index('measure')
+df.loc[screen_sq_errs,'measure'] = 'states_rmse'
+df = df.set_index('measure',append=True)
+
+# (2)
+screen_perc_errs = df.index.get_level_values('measure') == 'us_tot_perc_error'
+df.loc[screen_perc_errs,:] =  df.loc[screen_perc_errs,:].applymap(lambda x: x if pd.isna(x) else abs(float(x.strip('%'))))
+
+# Also, a slightly nicer name for the state rank
+df = df.reset_index('measure')
+screen_state_rank = df['measure'] == 'state_rank_mean_rank'
+df.loc[screen_state_rank,'measure'] = 'state_mean_rank'
+df = df.set_index('measure',append=True)
+
+# Now that we've fixed percentages, make everything a float
+df = df.astype(float)
+
+# Trim the weeks further into the future from the start_wk than we want to use, and sort the index for efficiency later
+screen = df.index.get_level_values('num_wks') <= nweeks
+df = df.loc[screen,:].sort_index()
+
+# Optionally, take out baseline(s) COVIDhub ensemble from the power rating process depending on policy choices
+l_excluded_models = []
+if not d_policy['include_baselines']: l_excluded_models += ['Baseline','COVIDhub-baseline']
+if not d_policy['include_ensemble']: l_excluded_models += ['COVIDhub-ensemble']
+cols = [c for c in df.columns if c not in l_excluded_models]
+df = df[cols]
+
+df.columns.name = 'Model' # This isn't carried through manipulations above?
+
+outstring = 'Generating working df of shape: '+str(df.shape)
+say(outstring,l_outs)
+
+# Calculate the power ratings for each start_wk/num_wks/measure/Model 4-tuple, where the power rating is done over all models present for the start_wk/num_wks/measure 3-tuple
+# df_power holds the power ratings
+df_power = df.apply(power_score,axis=1)
+df_power.columns.name = 'Model' # power_score doesn't know about name of series it returns
+
+# Make some aggregations depending on policy choices
+# dft is modified and used throughout; we make some worthwhile intermediate aggregations and store as we go.
+
+# First, create a single power rating for each Model/start_wk/num_wks tuple (averaging over all measures)
+dft = df_power.stack().unstack('measure')
+if d_policy['include_models_without_full_measure_set']:
+    dft = dft.fillna(0,axis=1)
+dft = dft.mean(skipna=False,axis=1) # It's actually a series at this point
+df_power_model_wk_pair = dft.unstack('Model')
+
+# Now, for each Model/start_wk 2-tuple, aggregate over the weeks (up to nweeks)
+
+# First, count the number of weeks including/after start_wk for which there is any data for each model
+s_model_wks = dft.unstack(level=['num_wks']).apply(lambda x: x.notna().sum(),axis=1)
+
+# Now for each start week, how many weeks data are expected?
+i = s_model_wks.index.get_level_values('start_wk').unique()
+max_weeks_expected = nweeks + 1 - np.maximum(i-32+nweeks,0)
+s_start_wk_max_wks = pd.Series(index=i,data=max_weeks_expected)
+
+# Now check if the weeks we counted as having data is sufficient
+screen_sufficient_weeks = (s_model_wks.unstack('Model').fillna(0).astype(int)
+                           .apply(lambda x: x.values == s_start_wk_max_wks.values).stack('Model'))
+i_model_wks = s_model_wks[screen_sufficient_weeks].index
+
+# Finally, apply the policies and aggregation
+dft = dft.unstack('num_wks')
+if not d_policy['include_models_without_all_weeks']:
+    dft = dft.loc[i_model_wks,:]
+dft = dft.mean(skipna=d_policy['generate_partial_results_for_recent_weeks'],axis=1).dropna(axis=0,how='all')
+
+df_power_model_start_wk = dft.unstack('Model')
+df_power_model_start_wk.index.name = 'start_wk'
+df_power_model_start_wk.columns.name = 'Model'
+
+# Generate a Model's lifetime average power ranking, over a specified set of weeks (can be all weeks of course)
+week_alpha = first_week
+week_omega = max(df_power_model_start_wk.index)+1
+weeks_included = list(range(week_alpha,week_omega))
+outstring = 'Generating an average of power ratings for models over a span of '+str(len(weeks_included))+' weeks.'
+say(outstring,l_outs)
+outstring = 'First week: '+str(first_week)+' (i.e., week starting '+str(d_wk_to_startdate[first_week])+')'
+say(outstring,l_outs)
+outstring = '  Covering the period of first week plus an additional '+str(nweeks)+' weeks (i.e., a total of '+str(nweeks+1)+' weeks)'
+say(outstring,l_outs)
+df_power_model = df_power_model_start_wk.loc[weeks_included,:].mean(axis=0).sort_values(ascending=False)
+df_power_model.name = 'Power_Rating'
+
+# We can also look at models over their whole lifetime (or, of course, a segment of it) and calculate each model's power rating for each specific measure.
+df_power_model_measure = df_power.stack().groupby(['Model','measure']).mean().unstack('measure')
+
+# We can also look at the range (e.g., max-min) of the power ratings of a model for the various measures
+# N.B. this is not currently written in the output file. But why aren't you using a notebook anyway?
+df_power_model_measure.dropna(axis=0,how='any').apply(lambda x: x.max() - x.min(),axis=1).sort_values()
+
+# We could also look at power rating by number of weeks out from initial forecast (i.e., agg by num_wks)
+df_power_model_num_wks = df_power.stack().groupby(['Model','num_wks']).mean().unstack()
+
+# Save the work
+output_filename_base = "covid_model_power_ratings"
+output_file_week_str = 'start_wk_'+str(first_week)+'-num_wks_'+str(nweeks+1)
+output_filename = output_filename_base+'-'+output_file_week_str+'-'+datestamp+'.xlsx'
+full_path_to_output_file = output_dir+output_filename
+
+# (Possibly more) complicated (than necessary) way to set up the index & output worksheet
+d_output_sheets = {1: {'df': df_power_model,
+     'sheet_name': 'Model_lifetime',
+     'desc': 'Power rating for each model over its full lifetime (single number for each model)'},
+ 2: {'df': df_power_model_start_wk.T, # the transpose makes for better viewing
+     'sheet_name': 'Model_weekly',
+     'desc': 'Power ratings for each model for each week projections were submitted (single number for each model for each week)'},
+ 3: {'df': df_power_model_measure,
+     'sheet_name': 'Model_measures',
+     'desc': 'Power ratings for each separate measure for each model over its full lifetime (n_measures number for each model)'},
+ 4: {'df': df_power_model_num_wks,
+     'sheet_name': 'Model_num_wks',
+     'desc': 'Power ratings for each model for increasing 0, 1, 2, etc. (0-indexed) week periods since a projection was submitted, averaged over that projection\'s full lifetime (n_weeks numbers per model)'},
+ 5: {'df': df_power_model_wk_pair,
+     'sheet_name': 'Model_week_pairs',
+     'desc': 'Power ratings for each model for increasing 0, 1, 2, etc. (0-indexed) week periods since a projection was submitted, for each week projections were submitted'},
+ 6: {'df': df_power,
+     'sheet_name': 'Raw_power_ratings',
+     'desc': 'Power ratings for every model, for each model/start_wk/num_wk/measure 4-tuple'}
+}
+l_sheet_names = [vd['sheet_name'] for vd in d_output_sheets.values()]
+l_descs = [vd['desc'] for vd in d_output_sheets.values()]
+
+df_index_sheet = pd.DataFrame(data=[l_sheet_names,l_descs],index=['Sheet name','Description']).T
+
+outstring = 'Saving power ratings in '+output_filename+'...'
+say(outstring,l_outs,term='')
+
+df_info_sheet = pd.DataFrame(l_outs)
+
+with pd.ExcelWriter(full_path_to_output_file) as writer:
+    df_info_sheet.to_excel(writer,sheet_name='Info',header=False,index=False)
+    df_index_sheet.to_excel(writer,sheet_name='Index',header=True,index=False)
+    for k, v in d_output_sheets.items():
+        sheet_name = v['sheet_name']
+        df = v['df']
+        df.to_excel(writer, sheet_name=sheet_name,float_format='%.2f')
+say('Done',l_outs,save=False)
